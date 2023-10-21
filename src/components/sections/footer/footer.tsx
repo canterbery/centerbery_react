2ddf309bf5c966e79b1a91584c3b2354bc90d795
canterbery <wotphantommail@gmail.com>
2023-10-21 14:30:49 +0300

    cb-0: + footer

---
+++ src/components/sections/footer/footer.tsx
@@ -0,0 +1,22 @@
+import { FooterMap } from "../../FooterMap";
+import styles from "./styles.module.css";
+const FooterSection = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h1
+          className={styles.desc_text}
+          style={{
+            paddingBottom: "20px",
+            paddingTop: "20px",
+          }}
+        >
+          Локація та розташування
+        </h1>
+        <FooterMap />
+      </div>
+    </div>
+  );
+};
+
+export { FooterSection };
